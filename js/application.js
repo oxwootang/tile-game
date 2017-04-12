d5d6252363cf95f65267fed290730a51500928cd
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-04-12 14:43:29 -0700

    Add move function to game object with helper function to test for valid up/down moves; next is to add left/right and to update the grid with each valid direction movement

--- js/application.js
+++ js/application.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 
   Mousetrap.bind("up", function() {
     console.log("up");
+    game.move("up");
   })
 
   Mousetrap.bind("down", function() {
     console.log("down");
+    game.move("down");
   })
 
   Mousetrap.bind("right", function() {
