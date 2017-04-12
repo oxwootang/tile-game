4d433abe40a9e8cbaa57c69a0db0348e4a3bc0e4
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-04-12 15:26:54 -0700

    Add play button and move mousetrap bindings to event delegation of play button

--- js/application.js
+++ js/application.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
 
-  Mousetrap.bind("up", function() {
-    console.log("up");
-    game.move("up");
-  })
+  $("button").on("click", function(e){
+    e.preventDefault();
+    console.log("Clicked play!");
 
-  Mousetrap.bind("down", function() {
-    console.log("down");
-    game.move("down");
-  })
+    Mousetrap.bind("up", function() {
+      console.log("up");
+      game.move("up");
+    });
 
-  Mousetrap.bind("right", function() {
-    console.log("right");
-    game.move("right");
-  })
+    Mousetrap.bind("down", function() {
+      console.log("down");
+      game.move("down");
+    });
 
-  Mousetrap.bind("left", function() {
-    console.log("left");
-    game.move("left");
-  })
+    Mousetrap.bind("right", function() {
+      console.log("right");
+      game.move("right");
+    });
+
+    Mousetrap.bind("left", function() {
+      console.log("left");
+      game.move("left");
+    });
+  });
 });
