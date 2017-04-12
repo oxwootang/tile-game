a3491cd45a2fcc4c4c1ec224bfdfd25bceba1d47
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-04-11 19:24:22 -0700

    Add mousetrap key-binding console response for all directions

--- js/application.js
+++ js/application.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
     console.log("up");
   })
 
+  Mousetrap.bind("down", function() {
+    console.log("down");
+  })
+
+  Mousetrap.bind("right", function() {
+    console.log("right");
+  })
+
+  Mousetrap.bind("left", function() {
+    console.log("left");
+  })
 });
